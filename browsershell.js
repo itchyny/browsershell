193d74666ec25a869d70b3431738bfcaf2ebd995
itchyny <neknekneknek.t@gmail.com>
2011-04-05 09:55:23 +0900

    .js added

--- browsershell.js
+++ browsershell.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+//#! /usr/bin/env node
 
 var http = require('http'),
     io = require('socket.io'),
@@ -8,65 +8,39 @@ var http = require('http'),
     net = require('net'),
     jsdom = require('jsdom');
 
-var conts = {},
+var contents = {},
     server = null,
-    content = fs.readFileSync(__dirname + '/index.html', 'utf8'),
-    document = jsdom.jsdom(content),
-    window = document.createWindow();
-
+    content = fs.readFileSync(__dirname + '/index.html', 'utf8');
 
 ['/main.js', '/socket.io.js', '/jquery-1.5.1.min.js', '/main.css']
-  .forEach(function (x) { conts[x] = fs.readFileSync(__dirname + x, 'utf8'); });
+  .forEach(function (x) { contents[x] = fs.readFileSync(__dirname + x, 'utf8'); });
 
 (function () {
   server = http.createServer(function (req, res) {
-    if(req.url === '/') {
+    if (req.url === '/') {
       res.writeHeader(200, {
         'Content-Type': 'text/html'
       });
       res.end(content);
-    } else if(/.*\.js/.test(req.url)) {
+    } else if (/.*\.js/.test(req.url)) {
       res.writeHeader(200, {
         'Content-Type': 'text/javascript'
       });
-      res.end(conts[req.url]);
-    } else if(/.*\.css/.test(req.url)) {
+      res.end(contents[req.url]);
+    } else if (/.*\.css/.test(req.url)) {
       res.writeHeader(200, {
         'Content-Type': 'text/css'
       });
-      res.end(conts[req.url]);
+      res.end(contents[req.url]);
     }
   });
   server.listen(8000);
 })();
-/*
-jsdom.env(content, ['http://code.jquery.com/jquery-1.5.min.js'], function (e, window) {
-  server = http.createServer(function (req, res) {
-    if(req.url === '/') {
-      res.writeHeader(200, {
-        'Content-Type': 'text/html'
-      });
-      res.end(window.document.innerHTML);
-    } else if(/.*\.js/.test(req.url)) {
-      res.writeHeader(200, {
-        'Content-Type': 'text/javascript'
-      });
-      res.end(conts[req.url]);
-    } else if(/.*\.css/.test(req.url)) {
-      res.writeHeader(200, {
-        'Content-Type': 'text/css'
-      });
-      res.end(conts[req.url]);
-    }
-  });
-  server.listen(8000);
-});
-*/
 
 (function () {
 
-socketinit();
-return;
+  socketinit();
+  return;
 
   function socketinit () {
     try {
@@ -80,14 +54,14 @@ return;
           client.on('message', function (mes) {
             client.broadcast(mes);
             var q = mes.replace(/ /g, '');
-            if(['ghci', 'zsh'/*, 'python'*/].indexOf(q) >= 0) {
+            if (['ghci', 'zsh'/*, 'python'*/].indexOf(q) >= 0) {
               prevspawn = spawn(mes, client);
               return;
             }
-            if(prevspawn) {
+            if (prevspawn) {
               try {
                 prevspawn.stdin.on('data', function (e, d) {
-                  if(e) throw e;
+                  if (e) throw e;
                   console.log(d.toString());
                 }).write(mes + '\n');
                 return;
@@ -97,16 +71,16 @@ return;
               }
             }
             child_process.exec(mes, function(e, stdout, stderr) {
-              if(stderr || e) {
+              if (stderr || e) {
                 spawn(mes, client);
                 return;
               }
-              if(stdout) {
+              if (stdout) {
                 client.send(stdout.toString());
                 client.broadcast(stdout.toString());
                 return;
               }
-              if(!stderr && !stdout) {
+              if (!stderr && !stdout) {
                 client.send('');
                 client.broadcast('');
                 return;
@@ -139,20 +113,20 @@ return;
           len = 0;
       return function (d) {
         stream += d.toString();
-        if(/execvp/.test(stream)) {
+        if (/execvp/.test(stream)) {
           client.send('command not found');
           client.broadcast('command not found');
           return;
         }
         ++len;
-        if(timer) clearTimeout(timer);
+        if (timer) clearTimeout(timer);
         timer = setTimeout(function () {
           client.send(stream);
           client.broadcast(stream);
           stream = '';
           len = 0;
         }, 100);
-        if(len > 1000) {
+        if (len > 1000) {
           clearTimeout(timer);
           client.send(stream);
           stream = '';
@@ -172,16 +146,16 @@ return;
 
   function toArray (s) {
     var i = 0, j = i, f = false, a = [];
-    while(s[i] === ' ' || s[i] === '\t') ++i;
-    while(s[++i] !== undefined) {
+    while (s[i] === ' ' || s[i] === '\t') ++i;
+    while (s[++i] !== undefined) {
       f = false;
-      while(s[i] !== ' ' && s[i] !== '\t' && s[i] !== undefined) {
-        if(s[i] === '"') { f = true; break; };
+      while (s[i] !== ' ' && s[i] !== '\t' && s[i] !== undefined) {
+        if (s[i] === '"') { f = true; break; };
         ++i;
       }
-      if(f) { ++i; ++j; while(s[i] !== '"' && s[i] !== undefined) ++i; }
+      if (f) { ++i; ++j; while (s[i] !== '"' && s[i] !== undefined) ++i; }
       a[a.length] = s.slice(j, i);
-      if(f) { ++i; }; j = i + 1;
+      if (f) { ++i; }; j = i + 1;
     }
     return a;
   };
